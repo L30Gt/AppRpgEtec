cfa313c54d236f2c3fc08c291a571f248054adf2
L30Gt <leeo12322@hotmail.com>
2024-05-22 17:37:03 -0300

    feat: add camera and gallery integration with Azure Blob storage

--- AppRpgEtec/AppShell.xaml.cs
+++ AppRpgEtec/AppShell.xaml.cs
@@ -1,16 +1,21 @@
-﻿using AppRpgEtec.Views.Armas;
+﻿using AppRpgEtec.ViewModels;
+using AppRpgEtec.Views.Armas;
 using AppRpgEtec.Views.Personagens;
 
 namespace AppRpgEtec
 {
     public partial class AppShell : Shell
     {
+        AppShellViewModel viewModel;
         public AppShell()
         {
             InitializeComponent();
 
             Routing.RegisterRoute("cadPersonagemView", typeof(CadastroPersonagemView));
             Routing.RegisterRoute("cadArmaView", typeof(CadastroArmaView));
+
+            viewModel = new AppShellViewModel();
+            BindingContext = viewModel;
 
             string login = Preferences.Get("UsuarioUsername", string.Empty);
             lblLogin.Text = $"Login: {login}";
